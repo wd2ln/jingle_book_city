3fc75c9ed6fb24102835fdbd6c42b78ccd923fd4
runoob <test@runoob.com>
2022-04-30 12:43:48 +0800

    项目搭建完成

--- src/main/java/com/zp/DemoApplication.java
+++ src/main/java/com/zp/DemoApplication.java
@@ -1,9 +1,11 @@
 package com.zp;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(value = "com.zp.dao")
 public class DemoApplication {
 
     public static void main(String[] args) {
