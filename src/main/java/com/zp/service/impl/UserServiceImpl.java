1e8e847ad190f783a0120f81672621fc6c5ce5a6
runoob <test@runoob.com>
2022-05-03 10:55:39 +0800

    添加登录功能

--- src/main/java/com/zp/service/impl/UserServiceImpl.java
+++ src/main/java/com/zp/service/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.zp.dao.UserMapper;
 import com.zp.entity.User;
 import com.zp.entity.UserExample;
 import com.zp.service.UserService;
+import com.zp.util.JasyptUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,12 +19,12 @@ public class UserServiceImpl implements UserService {
     public User login(User user) {
         UserExample userExample = new UserExample();
         userExample.createCriteria()
-                        .andUNameEqualTo(user.getuName())
-                                .andUPwdEqualTo(user.getuPwd());
+                        .andUNameEqualTo(user.getuName());
         try {
             List<User> users = userMapper.selectByExample(userExample);
             if (users.size()>0){
-                if (user.getuPwd().equals()){
+//                将拿到的密码与数据库的进行脱敏比对
+                if (user.getuPwd().equals(JasyptUtil.jie(users.get(0).getuPwd()))){
                     return users.get(0);
                 }else {
                     return null;
